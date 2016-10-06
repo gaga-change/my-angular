ba339cdfeddfa0156d76ba682f7207f8c79dc603
严俊东 <1172482914@qq.com>
2016-10-06 22:09:41 +0800

    12可更换主图片

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -6,9 +6,13 @@ angular.module('phoneDetail')
         controller: ['$routeParams','$http', function ($routeParams,$http) {
             //this.phoneId = $routeParams.phoneId;
             var self = this;
-
+            self.setImage = function(imgUrl){
+                self.phone.mainImageUrl = imgUrl;
+            };
             $http.get('phones/'+$routeParams.phoneId+".json").then(function(res){
                 self.phone=res.data;
+                self.setImage(self.phone.images[0]);
             });
+
         }]
     });
