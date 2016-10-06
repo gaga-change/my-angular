d23b3e39adcaf2aa254ec4a09176d3f6edb486ab
严俊东 <1172482914@qq.com>
2016-10-06 20:57:15 +0800

    10把简介变的更详细

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -2,8 +2,13 @@
 
 angular.module('phoneDetail')
     .component('phoneDetail', {
-        template: 'TBD : Detail View for <span> {{$ctrl.phoneId}}</span>',
-        controller: ['$routeParams', function ($routeParams) {
-            this.phoneId = $routeParams.phoneId;
+        templateUrl: "phone-detail/phone-detail.template.html",
+        controller: ['$routeParams','$http', function ($routeParams,$http) {
+            //this.phoneId = $routeParams.phoneId;
+            var self = this;
+
+            $http.get('phones/'+$routeParams.phoneId+".json").then(function(res){
+                self.phone=res.data;
+            });
         }]
     });
