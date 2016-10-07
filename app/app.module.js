17f24e8185b1f66aa61bde6ce8373bb1421baab9
严俊东 <1172482914@qq.com>
2016-10-07 09:07:38 +0800

    又冲突了

--- app/app.module.js
+++ app/app.module.js
@@ -3,5 +3,7 @@
 //Define the 'phonecatApp' module
 var phonecatApp = angular.module('phonecatApp',[
     // ...which depends on the `phoneList` module
+    'ngRoute',
+    'phoneDetail',
     'phoneList'
 ]);
