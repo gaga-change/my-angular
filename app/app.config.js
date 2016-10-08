d74cae86a49c5acf73025cb222d5128dd3538302
严俊东 <1172482914@qq.com>
2016-10-08 17:47:42 +0800

    扫尾

--- app/app.config.js
+++ app/app.config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
 angular.module('phonecatApp')
     .config(['$locationProvider' ,'$routeProvider',
         function config($locationProvider, $routeProvider) {
@@ -13,21 +12,4 @@ angular.module('phonecatApp')
             }).otherwise('/phones');
         }
     ]);
-=======
-angular.
-  module('phonecatApp').
-  config(['$locationProvider' ,'$routeProvider',
-    function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
 
-      $routeProvider.
-        when('/phones', {
-          template: '<phone-list></phone-list>'
-        }).
-        when('/phones/:phoneId', {
-          template: '<phone-detail></phone-detail>'
-        }).
-        otherwise('/phones');
-    }
-  ]);
->>>>>>> 95b8f2abef8802a91a097232d73b8966112041e6
--- app/app.module.js
+++ app/app.module.js
@@ -5,11 +5,6 @@ var phonecatApp = angular.module('phonecatApp',[
     // ...which depends on the `phoneList` module
     'ngRoute',
     'phoneDetail',
-<<<<<<< HEAD
     'phoneList'
-=======
-    'phoneList',
-    'core',
-    'ngAnimate'
->>>>>>> 95b8f2abef8802a91a097232d73b8966112041e6
+
 ]);
